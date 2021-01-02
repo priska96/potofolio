d8dcce95a641d0a8606bee7ae0a977acdeefac28
Priska Kohnen <priskakohnen@gmail.com>
2021-01-02 09:04:49 +0900

    refining detailpages

--- src/ProjectDetail.js
+++ src/ProjectDetail.js
@@ -1,18 +1,25 @@
 import nutritionplanner from './images/explain.mp4';
 import jabe from './images/jabe_small.png';
 import poilei from './images/poilei_small.png';
+import poilei_cookies from './images/poilei_cookies.png';
 import glowingkids from './images/glowingkids_small.png';
 import pscore_abuse from './images/pscore_abuse.png';
 import pscore_digital from './images/pscore_digital.png';
 import pscore_seminar from './images/pscore_seminar.png';
 import pscore_workshop from './images/pscore_workshop.png';
 import pscoreedu from './images/pscoreedu_small.png';
+import pscoreedu_register from './images/pscoreedu_register.png';
+import pscoreedu_1 from './images/pscoreedu_1.png';
+import pscoreedu_2 from './images/pscoreedu_2.png';
 import munscr from './images/munscr_small.png';
 import twotickets from './images/twotickets_small.png';
+import twotickets_redesign from './images/twotickets_redesign.png';
+import twotickets_filter from './images/twotickets_filter.png';
+import twotickets_vattenfall_1 from './images/twotickets_vattenfall_1.png';
 import './App.css';
 import React from 'react'
 import PropTypes from "prop-types";
-import {Button, Modal, Card, Container, Image, Jumbotron} from 'react-bootstrap';
+import {Button, Modal, Card, Container, Image, Jumbotron, ButtonGroup} from 'react-bootstrap';
 import me3 from "./images/me.png";
 
 
@@ -22,13 +29,12 @@ const data = {
             vid: nutritionplanner,
             cardTitle: "Meal Plan Generator",
             cardText: "The goal of this project was to create an algorithm to generate personalised meal plans, " +
-                "based on the users' input and to output them in a calendar overview. These meal plans support typical " +
-                "diets and eating habits  and provide a suitable nutrition intake. To create the meal plans and to " +
-                "choose the right  foods in a healthy measurement, an optimization problem was set up and solved by " +
-                "linear programming. The focus was more set on the logic instead on the design, which is why the " +
-                "page has only minimal styling to make it usable. Besides Django (Python) jQuery, CSS and Bootstrap " +
-                "were used to  implement this website."
-
+                "based on the users' input and to output them in a calendar overview as well as export them in a pdf " +
+                "file. These meal plans support typical diets and eating habits  and provide a suitable nutrition " +
+                "intake. To create the meal plans and to choose the right  foods in a healthy measurement, an " +
+                "optimization problem was set up and solved by linear programming. The focus was more set on the " +
+                "logic instead on the design, which is why the page has only minimal styling to make it usable. " +
+                "Besides Django (Python) jQuery, CSS and Bootstrap  were used to  implement this website."
         },
         jabe: {
             img: jabe,
@@ -39,7 +45,7 @@ const data = {
                 "the Facebook tracking pixel for marketing services.",
         },
         poilei: {
-                img: poilei,
+                img: poilei_cookies,
                 cardTitle: "Poilei",
                 cardText: "Poilei is an Italian shoe shop using Shopify. Since I only maintain the page I mostly fix " +
                     "minor errors in the front-end and edit the layout for the pages as well as integrate tracking " +
@@ -67,11 +73,13 @@ const data = {
                 buttonCode: ''
         },
         pscoreedu: {
-                img: pscoreedu,
+                imgs: [pscoreedu_1, pscoreedu_2, pscoreedu_register],
                 cardTitle: "PSCORE EDU",
-                cardText: "A WordPress website to provide tutoring classes for North Korean defectors (maintaining).",
-                buttonWebsite: 'http://edu.pscore.org/en/',
-                buttonCode: ''
+                cardText: "The education page from PSCORE is a WordPress website to provide tutoring classes for " +
+                    "North Korean defectors. Students and tutors register on the page and are matched. I mostly update " +
+                    "the 'Successful Tutoring Experiences' section as well as added a PHP script to add the registration" +
+                    "date to the database and make it visible in the WordPress admin overview.",
+                buttonCode: 'https://github.com/priska96/pscore/blob/master/member_registration_date.php'
             },
     munscr: {
             img: munscr,
@@ -84,12 +92,17 @@ const data = {
 
         },
     twotickets: {
-        img: twotickets,
+        imgs: [twotickets_redesign, twotickets_filter, twotickets_vattenfall_1],
         cardTitle: "TwoTickets.de",
-        cardText: "A Django website, that sells memberships to its users. Members can win " +
+        cardText: "TwoTickets.de sells memberships to its users. Registered members can win a pair of tickets for any " +
+            "event, whose organizer collaborates with TwoTickets.de. I maintain the back- and the front-end in terms " +
+            "of bug fixing and adding new features. My main projects were a redesign of landingpages, optimizing the " +
+            "filter and search functions, a redesign of the iFrame version for a affiliate company (Vattenfall)," +
+            " and a current tech-stack update project. " +
+            "" +
+            "A Django website, that sells memberships to its users. Members can win " +
             "a pair of tickets for various events (maintaining).",
-        buttonWebsite: 'https://twotickets.de',
-        buttonCode: ''
+        buttonAffiliate: 'https://vattenfall.de/freikarten',
     }
 };
 
@@ -170,23 +183,22 @@ class ProjectDetail extends React.Component {
         }
         return (
             <Modal show={this.state.show} onHide={this.handleClose} className="modal-page">
-                <div className="text-right">
+                <div className="text-right pr-3">
                     <Button variant="back" onClick={this.handleClose}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                             className="bi bi-chevron-double-left" viewBox="0 0 20 20">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                             className="bi bi-chevron-double-left" viewBox="0 0 30 30">
                             <path fillRule="evenodd"
                                   d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
                             <path fillRule="evenodd"
                                   d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
-                        </svg>
-                        Back
+                        </svg>Back
                     </Button>
                 </div>
                 <Jumbotron className="img-container2 bg-transparent">
                   <h1>{data[this.card].cardTitle}</h1>
                 <Container
-                    className="d-flex flex-lg-row flex-column justify-content-lg-start align-items-lg-center justify-content-center align-items-start aboutme">
-                        {media !== 'imgs'? <div className="me-image">{media}</div> :
+                    className="d-flex flex-lg-row flex-column flex-wrap justify-content-lg-start align-items-lg-center justify-content-center align-items-start aboutme">
+                        {media !== 'imgs'? <div className="image-box"><div className="project-img">{media}<div className="overlay-text"><span>Click to enlarge</span></div></div></div> :
                             <div className="image-box">
                                 {data[this.card].imgs.map((s, i) =>
                                     <div className="project-img" onClick={() => this.viewLightbox(s)}>
@@ -201,6 +213,12 @@ class ProjectDetail extends React.Component {
                         <p>{data[this.card].cardText}
                         </p>
                     </div>
+                        {data[this.card].buttonCode ?
+                            <ButtonGroup  className="justify-content-between"><Button variant="loading"
+                                    href={data[this.card].buttonCode} target="_blank">View Code</Button></ButtonGroup> : null}
+                        {data[this.card].buttonAffiliate ?
+                            <ButtonGroup  className="justify-content-between"><Button variant="loading"
+                                    href={data[this.card].buttonAffiliate} target="_blank">View Affiliate Website</Button></ButtonGroup> : null}
                 </Container>
             </Jumbotron>
                 <Lightbox show={this.state.showLightBox} src={this.state.imgSrc}/>
